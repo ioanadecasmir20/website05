f4cd1e027842e757136b5120988af95a69e3976d
ioanadecasmir20 <ioana@worldsecuritygroup.co.uk>
2025-04-17 10:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('LUCAT homepage loaded.');
+
+  // Toggle mobile menu
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const menu = document.getElementById('menu');
+
+  if (toggleBtn && menu) {
+    toggleBtn.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
+  }
 });
+
